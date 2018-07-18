a12e7c591e8b9cf6fcf4842ecbce575253bba54d
surender <93suren23@gmail.com>
2018-07-18 11:49:34 +0530

    Implement EditTextPreferenceConstaint

--- app/src/main/java/prefFragment.java
+++ app/src/main/java/prefFragment.java
@@ -1,18 +1,27 @@
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.util.prefs.PreferenceChangeEvent;
+import java.util.prefs.PreferenceChangeListener;
+
+import e.sura23.tables2.MainActivity;
 import e.sura23.tables2.R;
 
 /**
  * Created by SURA23 on 7/8/2018.
  */
 
-public class prefFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
-    @Override
+public class prefFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
+
+
+    String TAG = prefFragment.class.getSimpleName ();
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource (R.xml.pref_fragment);
 
@@ -27,8 +36,13 @@ public class prefFragment extends PreferenceFragmentCompat implements SharedPref
             if (preference instanceof ListPreference) {
                 String listPreferenceValueFromSharedPreferences = sharedPreferences.getString (preference.getKey (), "");
                 setListPreferenceSummary (preference, listPreferenceValueFromSharedPreferences);
+            } else if (preference instanceof EditTextPreference) {
+                String EditTextValueFromSharedPreference = sharedPreferences.getString (preference.getKey (), "");
+                setEditTextSummary (preference, EditTextValueFromSharedPreference);
             }
         }
+        Preference preference = findPreference (getString (R.string.edit_text_key));
+        preference.setOnPreferenceChangeListener (this);
     }
 
     public void setListPreferenceSummary(Preference preferences, String listPreferenceValue) {
@@ -38,6 +52,10 @@ public class prefFragment extends PreferenceFragmentCompat implements SharedPref
 
     }
 
+    public void setEditTextSummary(Preference preference, String EditTextValueFromSharedPreference) {
+        EditTextPreference editTextpreference = (EditTextPreference) preference;
+        editTextpreference.setSummary (EditTextValueFromSharedPreference);
+    }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
@@ -47,6 +65,9 @@ public class prefFragment extends PreferenceFragmentCompat implements SharedPref
         if (preference instanceof ListPreference) {
             String listPreferenceValueFromSharedPreferences = sharedPreferences.getString (preference.getKey (), "");
             setListPreferenceSummary (preference, listPreferenceValueFromSharedPreferences);
+        } else if (preference instanceof EditTextPreference) {
+            String editTextValueFromSharedPreferences = sharedPreferences.getString (preference.getKey (), "");
+            setEditTextSummary (preference, editTextValueFromSharedPreferences);
         }
 
     }
@@ -61,5 +82,30 @@ public class prefFragment extends PreferenceFragmentCompat implements SharedPref
     public void onDestroy() {
         super.onDestroy ();
         getPreferenceScreen ().getSharedPreferences ().unregisterOnSharedPreferenceChangeListener (this);
+    }
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+        Toast showerror = Toast.makeText (getContext (), "Enter values between 1 and 15", Toast.LENGTH_SHORT);
+        Log.d (TAG, "editTextConstraint");
+        if (preference.getKey ().equals (getString (R.string.edit_text_key))) {
+            String editTextValue = (String) newValue;
+            try {
+                Float value = Float.parseFloat (editTextValue);
+
+                if ((value <= 0)||(value > 15)) {
+                    showerror.show ();
+                    return false;
+                }
+
+            } catch (NumberFormatException nfe) {
+                showerror.show ();
+                return false;
+            }
+
+        }
+        return true;
     }
 }
